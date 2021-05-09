3cc599ab6e7321b85e8590642c725c659add895a
Gabriel g3 <gabrielfranccelino@gmail.com>
2021-05-09 10:34:26 -0300

    fix: warning modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { GlobalStyles } from 'global'
 import { Home } from 'pages/home'
 
+import ReactModal from 'react-modal'
 import { Provider } from 'react-redux'
 import store from 'store'
+
+ReactModal.setAppElement('#root')
 
 const App: React.FC = () => {
   return (
